03fc1d7b4cbc40bca81c590459a9b3364fc683a9
irfan <irfan27dec@gmail.com>
2019-12-19 19:20:12 +0530

    Added get call

---
+++ src/test/java/googleplacesearch/Basic.java
@@ -0,0 +1,40 @@
+package googleplacesearch;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+
+import static io.restassured.RestAssured.given;
+
+import org.testng.annotations.Test;
+import static org.hamcrest.Matchers.equalTo;
+
+public class Basic {
+	
+	/*public static void main(String args[]){
+		
+	}*/
+
+	@Test()
+	public void GetCallExample() {
+		
+		//Provide Base URL
+		RestAssured.baseURI = "https://maps.googleapis.com";
+		given().
+				/*header("","").
+				cookie("","").
+				body("").*/
+				param("location","-33.8670522,151.1957362").
+				param("radius","1500").
+				param("key","AIzaSyA03bO2x2fYHYfXEhiwD_9gx54QXsfVe-I").
+				when().
+				get("/maps/api/place/nearbysearch/json").
+				then().assertThat().statusCode(200).and().contentType(ContentType.JSON).and().
+				//body("results[0].geometry.location.lat",equalTo("-33.8585416"));
+				body("results[0].name",equalTo("Sydney")).and().
+				body("results[0].place_id", equalTo("ChIJP3Sa8ziYEmsRUKgyFmh9AQM")).and().
+				header("Server", "scaffolding on HTTPServer2");
+				
+
+	}
+
+}
